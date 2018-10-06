3a84ef02d831b74d20280307134765a67ccd66ff
SergioEric <sericmorales@gmail.com>
2018-10-06 15:13:23 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import * as firebase from 'firebase/app';
 
@@ -13,10 +14,12 @@ export class AppComponent {
   }
   title = 'LoginFire';
 
-  // two-way-data binding variables ng-model 
-  email:string ="" 
-  password:string= ""
+  userForm: FormGroup;
 
+  // two-way-data binding variables ng-model 
+  // email:string ="" 
+  // password:string= ""
+  model: any = {}; // model de usuario
   //log de respuesta
   resetInfo =''
   successMessage =''
@@ -26,7 +29,7 @@ export class AppComponent {
   crearUsuarioPorEmail(){
 
     //if(this.email.trim() == "" || this.password.trim() == ""){ return false;}// si hay campos vacios no creamos usuario
-    this.fireAuth.auth.createUserWithEmailAndPassword(this.email, this.password).then(res=>{
+    this.fireAuth.auth.createUserWithEmailAndPassword(this.model.email, this.model.password).then(res=>{
       // este metodo retorna una promesa javascript, metodo que usamos en vez de try catch
       // el response de firebase es un objeto con todos los atributos de este mismo, como displayname, email, idToken, etc
       console.log(`email desde firebase: ${res.user.email}`)
@@ -46,10 +49,10 @@ export class AppComponent {
 
   resetarContrasena(){
     this.successMessage ='' //limpio el label
-    this.fireAuth.auth.sendPasswordResetEmail(this.email)
+    this.fireAuth.auth.sendPasswordResetEmail(this.model.email)
 		.then(()=>{
 			//email sent
-			this.resetInfo =`peticion de contrasena enviada, habre tu email: ${this.email}`
+			this.resetInfo =`peticion de contrasena enviada, habre tu email: ${this.model.email}`
 		})
 		.catch(error=>{
 			//error ocurrido
@@ -57,7 +60,7 @@ export class AppComponent {
 		});
   }
   loguear(){
-    this.fireAuth.auth.signInWithEmailAndPassword(this.email, this.password).then(res=>{
+    this.fireAuth.auth.signInWithEmailAndPassword(this.model.email, this.model.password).then(res=>{
       this.loginMessage = 'usuario logueado con exito'
     })
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {AngularFireModule} from '@angular/fire'; //modulo bootstrap de firebase
-import { AngularFireAuthModule } from '@angular/fire/auth'
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment'; // la configuracion de firebase para nuestro proyecto
@@ -12,6 +13,7 @@ import { environment } from '../environments/environment'; // la configuracion d
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule
   ],
